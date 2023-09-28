a9f17f98ffcfed21b66a55d5f1a5b7394035154f
darthzedious <angel.bobotsov@gmail.com>
2023-09-29 01:38:41 +0300

    upload

---
+++ SoftUni-Fundamentals-2023/Exercises/Lists/ex_4_Number_Beggars.py
@@ -0,0 +1,14 @@
+list_of_numbers = input().split(", ")
+count_of_beggars = int(input())
+list_of_numbers_as_digit = []
+for i in list_of_numbers:
+    list_of_numbers_as_digit.append(int(i))
+final_list = []
+start_index = 0
+while start_index < count_of_beggars:
+    sum_for_current_beggar = 0
+    for i in range(start_index, len(list_of_numbers_as_digit), count_of_beggars):
+        sum_for_current_beggar += list_of_numbers_as_digit[i]
+    final_list.append(sum_for_current_beggar)
+    start_index += 1
+print(final_list)
