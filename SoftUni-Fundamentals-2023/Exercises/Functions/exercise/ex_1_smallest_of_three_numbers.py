878614e4dc6858d0d201e1d1ce32ee1dc35b7195
darthzedious <angel.bobotsov@gmail.com>
2023-10-08 17:34:34 +0300

    upload

---
+++ SoftUni-Fundamentals-2023/Exercises/Functions/exercise/ex_1_smallest_of_three_numbers.py
@@ -0,0 +1,8 @@
+def return_smallest(num1, num2, num3):
+    return min(num1, num2, num3)
+
+
+number_one = int(input())
+number_two = int(input())
+number_three = int(input())
+print(return_smallest(number_one, number_two, number_three))
