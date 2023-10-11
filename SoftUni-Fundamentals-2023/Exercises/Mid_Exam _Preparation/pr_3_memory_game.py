33df1cf9193610d1bfa3e2382700dbfb73e1535d
darthzedious <angel.bobotsov@gmail.com>
2023-10-12 01:10:46 +0300

    upload

--- SoftUni-Fundamentals-2023/Exercises/Mid_Exam _Preparation/pr_3_memory_game.py
+++ SoftUni-Fundamentals-2023/Exercises/Mid_Exam _Preparation/pr_3_memory_game.py
@@ -1,25 +1,34 @@
 def memory_game(elements):
+    moves = 0
     while True:
-        moves = 0
-        matching_elements_found = 0
         guess = input().split(" ")
         if "end" in guess:
             print("Sorry you lose :(")
-            print(elements)
-            break
-
-        if elements[int(guess[0])] == elements[int(guess[1])]:
-            elements.remove([int(guess[0])]), elements.remove([int(guess[1])])
-            matching_elements_found += 1
-            print(f"Congrats! You have found matching elements - {matching_elements_found}!")
-            continue
-        else:
+            return " ".join(elements)
+        first_element = int(guess[0])
+        second_element = int(guess[1])
+        if len(elements) <= 0:
+            return f"You have won in {moves} turns!"
+        elif elements[first_element] == elements[second_element]:
+            removed_elements = elements[first_element]
+            if first_element > second_element:
+                elements.pop(first_element)
+                elements.pop(second_element)
+                moves += 1
+            else:
+                elements.pop(second_element)
+                elements.pop(first_element)
+                moves += 1
+            print(f"Congrats! You have found matching elements - {removed_elements}!")
+        elif first_element == second_element or first_element not in range(len(elements)) or second_element\
+                not in range(len(elements)):
+            moves += 1
+            elements.insert(len(elements) // 2, "-{}a".format(moves))
+            elements.insert(len(elements) // 2, "-{}a".format(moves))
+            print("Invalid input! Adding additional elements to the board")
+        elif elements[first_element] != elements[second_element]:
             print("Try again!")
-
-
-
-        moves += 1
-    return elements
+            moves += 1
 
 
 sequence_of_elements = input().split(" ")
