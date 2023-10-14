70f600219ed4c513c0284ac35e8fa8fb123f5fc1
darthzedious <angel.bobotsov@gmail.com>
2023-10-14 17:29:56 +0300

    upload

--- SoftUni-Fundamentals-2023/Exercises/Mid_Exam _Preparation/pr_1_counter_strike.py
+++ SoftUni-Fundamentals-2023/Exercises/Mid_Exam _Preparation/pr_1_counter_strike.py
@@ -1,13 +1,18 @@
 initial_energy = int(input())
 count_won_battles = 0
-
+battle_counter = 0
 while True:
     distance = input()
-    current_energy = initial_energy
     if distance == "End of battle":
+        print(f"Won battles: {count_won_battles}. Energy left: {initial_energy}")
+        break
+    initial_energy -= int(distance)
+    if initial_energy < 0:
+        initial_energy = 0
+        print(f"Not enough energy! Game ends with {count_won_battles} won battles and {initial_energy} energy")
         break
     count_won_battles += 1
-    current_energy -= int(distance)
-    if current_energy <= 0:
-        print("Not enough energy! Game ends with {count} won battles and {energy} energy".)
+    battle_counter += 1
+    if battle_counter != 0 and battle_counter != 1 and battle_counter % 3 == 0:
+        initial_energy += count_won_battles
 
