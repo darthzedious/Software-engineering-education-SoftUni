a48910b4f572caa3c89da7b3c33494a0e92812d5
darthzedious <angel.bobotsov@gmail.com>
2023-10-22 00:23:43 +0300

    upload

---
+++ SoftUni-Fundamentals-2023/Exercises/Mid_Exam _Preparation/pr3_inventory.py
@@ -0,0 +1,21 @@
+journal = input().split(", ")
+command = input()
+while command != "Craft!":
+    order, item = (command.split(" - "))
+    if order == "Collect":
+        if item not in journal:
+            journal.append(item)
+    elif order == "Drop":
+        if item in journal:
+            journal.remove(item)
+    elif order == "Combine Items":
+        old_item, new_item = item.split(":")
+        if old_item in journal:
+            journal.insert(journal.index(old_item) + 1, new_item)
+    elif order == "Renew":
+
+        if item in journal:
+            journal.remove(item)
+            journal.append(item)
+    command = input()
+print(", ".join(journal))
