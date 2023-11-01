f7e815a84c33bfa0a9c38beabb117e155c6e26f9
darthzedious <angel.bobotsov@gmail.com>
2023-11-01 14:02:24 +0200

    upload

--- SoftUni-Fundamentals-2023/Exercises/Practical_tasks/linear_search_algorithm.py
+++ SoftUni-Fundamentals-2023/Exercises/Practical_tasks/linear_search_algorithm.py
@@ -1,19 +1,24 @@
-def linear_search(input_list, target):
+def binary_search(input_list, target):
     left = 0
     right = len(input_list) - 1
 
     while left <= right:
-        middle_index = len(input_list) // 2
+        middle_index = (left + right) // 2
         middle_element = input_list[middle_index]
 
         if middle_element == target:
             return middle_index
         if target > middle_element:
-            left = middle_element + 1
+            left = middle_index + 1
         else:
-            right = middle_element - 1
+            right = middle_index - 1
 
 
-my_list = [1, 3, 5, 7, 9, 11]
-target = 7
-print(linear_search(my_list, target))
+my_list = sorted([int(number) for number in input().split(", ")])
+our_target = int(input())
+print(f"When the list is sorted correctly the target number is at index: {binary_search(my_list, our_target)}")
+
+'#For this algorithm to work properly, the given list must be sorted from lower to higher' \
+  ', that is why we use the sorted function to ensure ourselves.'
+'#We insert a list and a target, ' \
+  'then we call the function to print the index where lays the target we are searching for.'
