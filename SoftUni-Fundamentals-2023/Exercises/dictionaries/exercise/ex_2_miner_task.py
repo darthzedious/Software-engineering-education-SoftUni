8471dca26cf104755366eeced07224dcf9c81150
darthzedious <angel.bobotsov@gmail.com>
2023-11-06 01:15:07 +0200

    upload

---
+++ SoftUni-Fundamentals-2023/Exercises/dictionaries/exercise/ex_2_miner_task.py
@@ -0,0 +1,11 @@
+resources = {}
+while True:
+    resource = input()
+    if resource == "stop":
+        break
+    quantity = int(input())
+    if resource not in resources.keys():
+        resources[resource] = 0
+    resources[resource] += quantity
+for resource, quantity in resources.items():
+    print(f"{resource} -> {quantity}")
