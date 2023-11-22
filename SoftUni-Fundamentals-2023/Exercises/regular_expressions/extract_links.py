f4963258ae7e40ec105f4277b4e40743e648ec53
darthzedious <angel.bobotsov@gmail.com>
2023-11-22 02:51:03 +0200

    upload

--- SoftUni-Fundamentals-2023/Exercises/regular_expressions/extract_links.py
+++ SoftUni-Fundamentals-2023/Exercises/regular_expressions/extract_links.py
@@ -1,6 +1,6 @@
 import re
 line = input()
-pattern = r"(w{3}\.([A-Za-z\d\-]+)\.([a-z\.]+))"
+pattern = r"(w{3}\.([A-Za-z\d\-]+)\.([a-z\.]+))" # the whole email is group 1
 while line:
     match = re.search(pattern, line)
     if match:
