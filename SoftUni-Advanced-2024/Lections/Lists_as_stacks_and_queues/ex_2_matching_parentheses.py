d589830ab53da586f93a3cda80e81a505209a0fe
darthzedious <angel.bobotsov@gmail.com>
2024-01-08 03:05:06 +0200

    upload second exercise

---
+++ SoftUni-Advanced-2024/Lections/Lists_as_stacks_and_queues/ex_2_matching_parentheses.py
@@ -0,0 +1,8 @@
+expression = input()
+stack = []
+for i in range(len(expression)):
+    if expression[i] == "(":
+        stack.append(i)
+    if expression[i] == ")":
+        start_index = stack.pop()
+        print(expression[start_index:i+1])
