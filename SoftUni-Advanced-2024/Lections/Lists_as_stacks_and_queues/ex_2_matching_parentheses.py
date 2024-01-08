081c1d8508b0648c800e73eb1709627aceed0b65
darthzedious <angel.bobotsov@gmail.com>
2024-01-08 03:07:07 +0200

    comment

--- SoftUni-Advanced-2024/Lections/Lists_as_stacks_and_queues/ex_2_matching_parentheses.py
+++ SoftUni-Advanced-2024/Lections/Lists_as_stacks_and_queues/ex_2_matching_parentheses.py
@@ -4,5 +4,5 @@ for i in range(len(expression)):
     if expression[i] == "(":
         stack.append(i)
     if expression[i] == ")":
-        start_index = stack.pop()
+        start_index = stack.pop() # the index of the last "("
         print(expression[start_index:i+1])
