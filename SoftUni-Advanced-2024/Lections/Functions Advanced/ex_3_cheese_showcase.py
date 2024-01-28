b4e8d1ee75b101792980ae933d0786eaf6ab70de
darthzedious <angel.bobotsov@gmail.com>
2024-01-28 02:55:42 +0200

    upload

---
+++ SoftUni-Advanced-2024/Lections/Functions Advanced/ex_3_cheese_showcase.py
@@ -0,0 +1,11 @@
+def sorting_cheeses(**kwargs):
+    pass
+
+
+print(
+    sorting_cheeses(
+        Parmesan=[102, 120, 135],
+        Camembert=[100, 100, 105, 500, 430],
+        Mozzarella=[50, 125],
+    )
+)
