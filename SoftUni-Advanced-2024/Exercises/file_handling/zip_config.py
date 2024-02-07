eff04c138b9da1ef8fe1bdb111a76cbc76fbc6c8
darthzedious <angel.bobotsov@gmail.com>
2024-02-07 17:34:54 +0200

    upload

--- SoftUni-Advanced-2024/Exercises/file_handling/zip_config.py
+++ SoftUni-Advanced-2024/Exercises/file_handling/zip_config.py
@@ -2,7 +2,7 @@ import zipfile
 import os
 
 # Define the files you want to include in the zip file
-files_to_zip = []  #'other_folder/another_file.py'
+files_to_zip = ['01_even_lines.py', '02_line_numbers.py', '03_file_manipulator.py', '04_directory_traversal.py']  #'other_folder/another_file.py'
 
 # Specify the name of the zip file
 zip_file_name = 'homework.zip'
