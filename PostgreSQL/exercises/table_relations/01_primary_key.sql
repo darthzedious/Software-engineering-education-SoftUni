e94cc6e96ca1e52428084aeb93cce01c43a3c785
darthzedious <angel.bobotsov@gmail.com>
2024-06-03 04:49:23 +0300

    upload

---
+++ PostgreSQL/exercises/table_relations/01_primary_key.sql
@@ -0,0 +1,11 @@
+CREATE TABLE products(
+	product_name VARCHAR(100)
+);
+
+INSERT INTO products(product_name)
+VALUES('Broccoli'),('Shampoo'), ('Toothpaste'),('Candy');
+
+ALTER TABLE products
+ADD COLUMN "id" SERIAL PRIMARY KEY
+
+SELECT * FROM products
