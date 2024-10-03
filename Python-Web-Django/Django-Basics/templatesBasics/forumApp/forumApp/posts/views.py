64312b88e8610dd4ff406e1c6d24cf8960228629
darthzedious <angel.bobotsov@gmail.com>
2024-10-03 16:10:18 +0300

    view update

--- Python-Web-Django/Django-Basics/templatesBasics/forumApp/forumApp/posts/views.py
+++ Python-Web-Django/Django-Basics/templatesBasics/forumApp/forumApp/posts/views.py
@@ -1,5 +1,8 @@
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from forumApp.posts.forms import SearchForm, PostCreateForm, PostDeleteForm
+from forumApp.posts.models import Post
 
 
 def index(request):
@@ -11,24 +14,58 @@ def index(request):
 
 
 def dashboard(request):
+    form = SearchForm(request.GET)
+    posts = Post.objects.all()
 
+    if request.method =="GET":
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            posts = posts.filter(title__icontains=query)
 
     context = {
-
+        "posts": posts,
+        "form": form,
     }
 
     return render(request, 'posts/dashboard.html', context)
 
 
 def add_post(request):
+    form = PostCreateForm(request.POST or None)
+
+    if request.method =="POST":
+        if form.is_valid():
+            form.save()
+            return redirect('dash')
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, 'posts/add-post.html', context)
+def edit_post(request, pk: int):
     pass
 
+def  details_page(request, pk: int):
+    post = Post.objects.get(pk=pk)
 
-def edit_post(request):
-    pass
+    context = {
+        "post": post,
+    }
 
-def  details_page(request):
-    pass
+    return render(request, 'posts/details-post.html', context)
 
-def delete_post(request):
-    pass
+def delete_post(request, pk: int):
+    post = Post.objects.get(pk=pk)
+    form = PostDeleteForm(instance=post)
+
+    if request.method == "POST":
+        post.delete()
+        return redirect('dash')
+
+    context = {
+        "form": form,
+        "post": post,
+    }
+
+    return render(request, "posts/delete-template.html", context)
