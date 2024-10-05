00055e2ef899dc135a7e15a1aabc56646c34c746
darthzedious <angel.bobotsov@gmail.com>
2024-10-05 13:39:31 +0300

    create the register and log in forms, views and templates

---
+++ Python-Web-Django/Django-Basics/financeDjango/financeDjango/home_menu_app/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+
+from financeDjango.home_menu_app.models import User
+
+
+class RegisterForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                # 'class': 'form-control',
+                'placeholder': 'Username',
+            }
+        )
+    )
+
+    password = forms.CharField(
+        max_length=50,
+        widget=forms.PasswordInput(
+            attrs={
+                # 'class': 'form-control',
+                'placeholder': 'Password',
+
+            }
+        )
+    )
--- Python-Web-Django/Django-Basics/financeDjango/financeDjango/home_menu_app/views.py
+++ Python-Web-Django/Django-Basics/financeDjango/financeDjango/home_menu_app/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+from financeDjango.home_menu_app.forms import LoginForm, RegisterForm
+
 
 # Create your views here.
 def home_view(request):
@@ -8,7 +13,44 @@ def stock_operations_menu(request):
     return render(request, 'home_menu/stock_shares_operation_menu.html')
 
 def log_in_view(request):
-    return render()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home_view')
+            else:
+                messages.error(request, 'Invalid username or password')
+
+    else:
+        form = LoginForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'home_menu/log_in.html', context)
 
 def register_view(request):
-    pass
+    if request.method =='POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            messages.success(request, 'Registration successful! You are now logged in.')
+            return redirect('home_view')
+        else:
+            messages.error(request, 'Registration failed. Please try again.')
+    else:
+        form = RegisterForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'home_menu/register.html', context)
