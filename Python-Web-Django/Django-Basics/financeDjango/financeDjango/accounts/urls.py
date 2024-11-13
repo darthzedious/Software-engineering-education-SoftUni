2f7b42e8bf15862ddfbf14b0451fb2e3f913a5b4
darthzedious <angel.bobotsov@gmail.com>
2024-11-13 22:49:23 +0200

    fix: fbv auth views to cbv

--- Python-Web-Django/Django-Basics/financeDjango/financeDjango/accounts/urls.py
+++ Python-Web-Django/Django-Basics/financeDjango/financeDjango/accounts/urls.py
@@ -4,9 +4,9 @@ from financeDjango.accounts import views
 
 urlpatterns = [
 
-    path('register/', views.register_view, name='register'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
 
-    path('login/', views.login_view, name='login'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
 
     path('logout/', views.logout_view, name='logout'),
     path('user/', views.LoadProfile.as_view(), name='profile'),
--- Python-Web-Django/Django-Basics/financeDjango/financeDjango/accounts/views.py
+++ Python-Web-Django/Django-Basics/financeDjango/financeDjango/accounts/views.py
@@ -1,53 +1,68 @@
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import  login, logout, get_user_model
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import , CreateView, DetailView
 
 from financeDjango.accounts.forms import LoginForm, RegisterForm
 
-LoginView
+UserModel = get_user_model()
 # Create your views here.
-def login_view(request):
-    if request.method == "POST":
-        form = LoginForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('profile')  # redirect to homepage after successful login
-            else:
-                messages.error(request, "Invalid username or password")
-    else:
-        form = LoginForm()
+# def login_view(request):
+#     if request.method == "POST":
+#         form = LoginForm(request, data=request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             password = form.cleaned_data.get('password')
+#             user = authenticate(request, username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('profile')  # redirect to homepage after successful login
+#             else:
+#                 messages.error(request, "Invalid username or password")
+#     else:
+#         form = LoginForm()
+#
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'accounts_templates/log_in.html', context)
 
-    context = {
-        'form': form,
-    }
-    return render(request, 'accounts_templates/log_in.html', context)
+class UserLoginView(LoginView):
+    template_name = 'accounts_templates/log_in.html'
 
-def register_view(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('profile')  # redirect to homepage after successful registration
-    else:
-        form = RegisterForm()
+# def register_view(request):
+#     if request.method == "POST":
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('profile')  # redirect to homepage after successful registration
+#     else:
+#         form = RegisterForm()
+#
+#     context = {
+#         'form': form,
+#     }
+#
+#     return render(request, 'accounts_templates/register.html', context)
 
-    context = {
-        'form': form,
-    }
+class UserRegisterView(CreateView):
+    model = UserModel
+    form_class = RegisterForm
+    template_name = 'accounts_templates/register.html'
+    success_url = reverse_lazy('profile')
 
-    return render(request, 'accounts_templates/register.html', context)
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+
+        return response
 
 def logout_view(request):
     logout(request)
     return redirect('home')
 
 
-class LoadProfile(TemplateView):
+class LoadProfile(DetailView):
     template_name = 'accounts_templates/user_profile.html'
